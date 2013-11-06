146661bd3d42e1f3af14d47aebc46e6224bc43c9
Juniorchen <Juniorchen2010@163.com>
2013-11-06 09:39:16 +0800

    11-6

---
+++ testStoryBoard/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  testStoryBoard
+//
+//  Created by mitbbs on 10/29/13.
+//  Copyright (c) 2013 Juniorchen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ testStoryBoardTests/testStoryBoardTests.m
@@ -0,0 +1,34 @@
+//
+//  testStoryBoardTests.m
+//  testStoryBoardTests
+//
+//  Created by mitbbs on 10/29/13.
+//  Copyright (c) 2013 Juniorchen. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface testStoryBoardTests : XCTestCase
+
+@end
+
+@implementation testStoryBoardTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
